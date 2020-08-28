adbf69635544ceb9f6007dd3cccb6f1b9b20d66f
Harsh Kant <harshkant87@gmail.com>
2020-08-28 18:26:14 +0530

    adding books routes

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+
 
 
 app.set('view engine','ejs')
@@ -33,6 +35,8 @@ db.once('open', () => console.log('Connected to db'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
+
 
 
 
